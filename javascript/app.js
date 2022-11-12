ccd225736aa7e60515cd934bad02c980805cceae
Fernando Soares <dotfernando@gmail.com>
2022-11-12 01:29:04 -0300

    refreshing
    atualizando layout

--- javascript/app.js
+++ javascript/app.js
@@ -5,40 +5,28 @@ hljs.highlightAll();
 let container = document.querySelector("#grid-container");
 let html = '';
 
-fetch('https://api.dribbble.com/v2/user/shots?access_token=627b0fac077b5b6f3dcafec723409a4eb98a2b09456afee83b804d54655f2967')
-.then(response => response.json())
-.then(data => {
-  data.forEach(shot => {
-    console.log(shot)
-    let shotHtml = `
-      <div class="slot">
-        <a href="${shot.html_url}" target="_blank" title="${shot.title}">
-          <figure>
-            <img src="${shot.images.normal}" alt="${shot.title}" loading="lazy" width="400" height="300" data-id="">
-          </figure>
-        </a>
-      </div>
-    `;
-
-    html += shotHtml;
-  });
-
-  container.innerHTML = html;
-});
-
-/*
 if (container) {
+  fetch('https://api.dribbble.com/v2/user/shots?access_token=627b0fac077b5b6f3dcafec723409a4eb98a2b09456afee83b804d54655f2967')
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(shot => {
+      let shotHtml = `
+        <div class="slot">
+          <a href="${shot.html_url}" target="_blank" title="${shot.title}">
+            <figure>
+              <img src="${shot.images.normal}" alt="${shot.title}" loading="lazy" width="400" height="300" data-id="${shot.id}">
+            </figure>
+          </a>
+        </div>
+      `;
 
-  jribbble.shots({token: "627b0fac077b5b6f3dcafec723409a4eb98a2b09456afee83b804d54655f2967", per_page: 12}, 
-  function(shots) {
-    const filteredShots = shots.filter(shot => shot.tags.includes('ui') && shot.animated !== true);
+      html += shotHtml;
+    });
 
-    const reducedShots = filteredShots.reduce((html, shot, item) => html + '<div class="slot"><a href="'+  shot.html_url + '" target="_blank"><figure><img width="400" height="300" loading="lazy" alt="'+ shot.title +'" data-id="' + item + '" src="' + shot.images.normal + '"></figure></a></div>', "");
-
-    // put on html
-    container.innerHTML = reducedShots;
+    container.innerHTML = html;
   });
-}*/
+}
+
 
 // themes
 
