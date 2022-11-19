edfbe55ceb977f0b2905d959c7c2146b46af04ab
Fernando Soares <dotfernando@gmail.com>
2022-11-19 16:51:55 -0300

    service work teste 2

--- javascript/app.js
+++ javascript/app.js
@@ -55,44 +55,12 @@ switcher.addEventListener("click", function () {
   }
 });
 
-// Service Work
-let cacheName = 'v1.0';
+// register service work
 
-let cacheAssets = [
-    '/index.html',
-    '/sobre/index.html',
-    '/links/index.html',
-    '/notes/index.html',
-    '/javascript/app.js',
-    '/css/main.css',
-    '/uploads/',
-    '/images/',
-    '/fonts/'
-]
-
-// installation
-self.addEventListener('install', e => {
-      console.log('Service Worker: Installed');
-      e.waitUntil(
-            caches
-              .open(cacheName)
-              .then(cache => {
-                console.log('Service Worker: Caching Files');
-                cache.addAll(cacheAssets);
-              })
-              .then(() => self.skipWaiting())
-          );
-
-});    
-
-// activation
-self.addEventListener('activate', e => {
-    console.log('Service Worker: Installed');
-}); 
-
-// fetch
-self.addEventListener('fetch', e => {
-      console.log('Service Worker: Fetching');
-        e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
-      
-});
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/javascript/sw.js').then(function(reg) {
+    console.log('Yey!', reg);
+  }).catch(function(err) {
+    console.log('Boo!', err);
+  });
+}
