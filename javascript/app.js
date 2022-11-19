12839843fa887b9606bb15a99323e811af968192
Fernando Soares <dotfernando@gmail.com>
2022-11-19 16:26:27 -0300

    service work test

--- javascript/app.js
+++ javascript/app.js
@@ -1,5 +1,3 @@
-//hljs.initHighlightingOnLoad();
-
 hljs.highlightAll();
 
 let container = document.querySelector("#grid-container");
@@ -55,4 +53,46 @@ switcher.addEventListener("click", function () {
     localStorage.setItem("theme", "dark");
     metaColor.setAttribute("content", "#161821")
   }
+});
+
+// Service Work
+let cacheName = 'v1.0';
+
+let cacheAssets = [
+    '/index.html',
+    '/sobre/index.html',
+    '/links/index.html',
+    '/notes/index.html',
+    '/javascript/app.js',
+    '/css/main.css',
+    '/uploads/',
+    '/images/',
+    '/fonts/'
+]
+
+// installation
+self.addEventListener('install', e => {
+      console.log('Service Worker: Installed');
+      e.waitUntil(
+            caches
+              .open(cacheName)
+              .then(cache => {
+                console.log('Service Worker: Caching Files');
+                cache.addAll(cacheAssets);
+              })
+              .then(() => self.skipWaiting())
+          );
+
+});    
+
+// activation
+self.addEventListener('activate', e => {
+    console.log('Service Worker: Installed');
+}); 
+
+// fetch
+self.addEventListener('fetch', e => {
+      console.log('Service Worker: Fetching');
+        e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+      
 });
